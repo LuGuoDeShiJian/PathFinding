3bd84eb7438e516d43165d4dd0143bfa9e85fcd3
路过的时间 <luguodeshijian@gmail.com>
2025-03-12 15:52:55 +0800

    添加cpp文件关联和优化枚举注释

--- include/core/DiagonalMovement.h
+++ include/core/DiagonalMovement.h
@@ -1,8 +1,9 @@
 #pragma once
 namespace luguo::PathFind
 {
+    //  定义一个枚举类DiagonalMovement，用于表示对角线移动的策略
     enum class DiagonalMovement
-    {                        //  定义一个枚举类DiagonalMovement，用于表示对角线移动的策略
+    {
         Always,              //  总是允许对角线移动
         Never,               //  从不允许对角线移动
         IfAtMostOneObstacle, //  仅当最多有一个障碍物时允许对角线移动
